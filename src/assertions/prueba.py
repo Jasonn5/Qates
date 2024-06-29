d3d91a8338da04246291904ddea409d98e9305e1
Jeyson Erik Valdivia Bernal <jasson.n021@gmail.com>
2024-06-29 12:28:33 -0400

    Get_Presentations
    Added automations to next test cases:
    -Verify that the endpoint returns a successful response (status code 200)
    -Successfully get meetings and validate the JSON schema
    -Verify that the response is in JSON format
    -Verify that meetings cannot be obtained without authentication

--- src/assertions/prueba.py
+++
@@ -1,5 +0,0 @@
-import pytest
-
-def assert_status_code(response, status_code):
-    assert response.status_code == status_code
-
--- tests/presentations/test_GetPresentations.py
+++ tests/presentations/test_GetPresentations.py
@@ -1,9 +1,31 @@
-from src.assertions.assetrion_schemas import assert_schema_presentation
+from src.assertions.assertion_schemas import assert_schema_presentation
 import requests
 from config import  BASE_URI
+from src.assertions.assertion_status import assert_status_code_ok, assert_status_code_unauthorized
 
 
 def test_get_meetings_success(get_headers):
     url = f"{BASE_URI}/Meeting?select=id,name,status,dateStart,dateEnd,dateStartDate,dateEndDate,parentId,parentType,parentName,createdById,assignedUserId,assignedUserName&maxSize=20&offset=0&orderBy=dateStart&order=desc"
     response = requests.get(url, headers=get_headers("jeyson", "Testing.123!"))
+    assert_status_code_ok(response)
+
+def test_get_meetings_schema_validation(get_headers):
+    url = f"{BASE_URI}/Meeting?select=id,name,status,dateStart,dateEnd,dateStartDate,dateEndDate,parentId,parentType,parentName,createdById,assignedUserId,assignedUserName&maxSize=20&offset=0&orderBy=dateStart&order=desc"
+    response = requests.get(url, headers=get_headers("jeyson", "Testing.123!"))
     assert_schema_presentation(response.json())
+
+def test_get_meetings_response_format(get_headers):
+    url = f"{BASE_URI}/Meeting?select=id,name,status,dateStart,dateEnd,dateStartDate,dateEndDate,parentId,parentType,parentName,createdById,assignedUserId,assignedUserName&maxSize=20&offset=0&orderBy=dateStart&order=desc"
+    response = requests.get(url, headers=get_headers("jeyson", "Testing.123!"))
+    assert response.headers['Content-Type'] == 'application/json'
+
+def test_get_meetings_unauthorized():
+    url = f"{BASE_URI}/Meeting"
+    response = requests.get(url)
+    assert_status_code_unauthorized(response)
+
+def test_get_meetings_invalid_auth(get_headers):
+    url = f"{BASE_URI}/Meeting"
+    headers = get_headers("invalid_user", "invalid_password")
+    response = requests.get(url, headers=headers)
+    assert_status_code_unauthorized(response)
