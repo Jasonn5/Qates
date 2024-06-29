a49a2b0709ec902aa42c3d4e304a03cf60b5100e
Jeyson Erik Valdivia Bernal <jasson.n021@gmail.com>
2024-06-29 12:28:33 -0400

    Get_Presentations
    Added automations to next test cases:
    -Verify that the endpoint returns a successful response (status code 200)
    -Successfully get meetings and validate the JSON schema
    -Verify that the response is in JSON format
    -Verify that meetings cannot be obtained without authentication

--- tests/login/test_login.py
+++ tests/login/test_login.py
@@ -1,40 +1,40 @@
 import requests
 import pytest
-from src.assertions.prueba import assert_status_code
+from src.assertions.assertion_status import assert_status_code_ok, assert_status_code_unauthorized
 from config import BASE_URI
 
 
 def test_get_login_success(get_headers):
     response = requests.get(f'{BASE_URI}/App/user', headers=get_headers("jeyson", "Testing.123!"))
     print(response)
-    assert_status_code(response, 200)
+    assert_status_code_ok(response)
 
 def test_get_login_invalid_username(get_headers):
     response = requests.get(f'{BASE_URI}/App/user', headers=get_headers("invalid_user", "Testing.123!"))
-    assert_status_code(response, 401)
+    assert_status_code_unauthorized(response)
 
 
 def test_get_login_invalid_password(get_headers):
     response = requests.get(f'{BASE_URI}/App/user', headers=get_headers("jeyson", "invalid_password"))
-    assert_status_code(response, 401)
+    assert_status_code_unauthorized(response)
 
 
 def test_get_login_invalid_username_and_password(get_headers):
     response = requests.get(f'{BASE_URI}/App/user', headers=get_headers("invalid_user", "invalid_password"))
-    assert_status_code(response, 401)
+    assert_status_code_unauthorized(response)
 
 def test_login_empty_fields(get_headers):
     response = requests.get(f'{BASE_URI}/App/user', headers=get_headers("", ""))
-    assert_status_code(response, 401)
+    assert_status_code_unauthorized(response)
 
 
 def test_login_empty_user(get_headers):
     response = requests.get(f'{BASE_URI}/App/user', headers=get_headers("", "Testing.123!"))
-    assert_status_code(response, 401)
+    assert_status_code_unauthorized(response)
 
 
 def test_login_empty_password(get_headers):
     response = requests.get(f'{BASE_URI}/App/user', headers=get_headers("jeyson", ""))
-    assert_status_code(response, 401)
+    assert_status_code_unauthorized(response)
 
 
