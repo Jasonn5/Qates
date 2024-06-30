640f728e1bc826eb7c00f32109724ae1f7b59ea3
Jeyson Erik Valdivia Bernal <jasson.n021@gmail.com>
2024-06-30 09:19:54 -0400

    SCRUM_4_get_meetings_pagination
    This commit adds a new test for pagination and an additional assertion function.
    
    Changes Made
    src\assertions\assertion_comparison.py
    
    Added the function assert_equal_to(value1, value2) to ensure two values are equal.
    tests\presentations\test_GetPresentations.py
    
    Added a new test test_get_meetings_pagination to verify the pagination functionality:
    Fetches meetings with and without an offset.
    Checks that the status code is 200 for both responses.
    Ensures the number of meetings does not exceed the maximum size.
    Compares the first item of the offset list to the expected item from the non-offset list.

--- tests/presentations/test_GetPresentations.py
+++ tests/presentations/test_GetPresentations.py
@@ -38,3 +38,29 @@ def test_get_meetings_invalid_auth(get_headers):
     headers = get_headers("invalid_user", "invalid_password")
     response = requests.get(url, headers=headers)
     assert_status_code_unauthorized(response)
+
+
+def test_get_meetings_pagination(get_headers):
+    offset = 1
+    max_size = 2
+    url_with_offset = f"{BASE_URI}/Meeting?select=id,name,status,dateStart,dateEnd,dateStartDate,dateEndDate,parentId,parentType,parentName,createdById,assignedUserId,assignedUserName&maxSize={max_size}&offset={offset}&orderBy=dateStart&order=desc"
+    url_without_offset = f"{BASE_URI}/Meeting?select=id,name,status,dateStart,dateEnd,dateStartDate,dateEndDate,parentId,parentType,parentName,createdById,assignedUserId,assignedUserName&maxSize={max_size}&offset=0&orderBy=dateStart&order=desc"
+    headers = get_headers("jeyson", "Testing.123!")
+    response_with_offset = requests.get(url_with_offset, headers=headers)
+    response_without_offset = requests.get(url_without_offset, headers=headers)
+
+    assert_status_code_ok(response_with_offset)
+    assert_status_code_ok(response_without_offset)
+
+    data_with_offset = response_with_offset.json()
+    data_without_offset = response_without_offset.json()
+
+    data_list_with_offset = data_with_offset["list"]
+    data_list_without_offset = data_without_offset["list"]
+
+    assert_less_than_or_equal_to(len(data_list_with_offset), max_size)
+
+    if len(data_list_without_offset) > offset:
+        expected_first_item_with_offset = data_list_without_offset[offset]
+        actual_first_item_with_offset = data_list_with_offset[0]
+        assert_equal_to(actual_first_item_with_offset["id"], expected_first_item_with_offset["id"])
