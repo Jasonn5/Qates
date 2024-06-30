3e8b861315871392b0f7b40436417f5a0c8eec31
AlyGASS <alisonga15aprilacss@gmail.com>
2024-06-30 13:00:49 -0400

    Refractored global variable calls_param to CALL_PARAM and the file assert_headers to assertion_headers

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 BASE_URI = 'https://espo.spartan-soft.com/api/v1'
 USERNAME = "admin"
 PASSWORD = "admin"
-calls_params = {
+CALL_PARAM = {
     'select': 'assignedUserId%2CassignedUserName%2CdateStart%2Cstatus%2CparentId%2CparentType%2CparentName%2Cname',
     'maxsize': '20',
     'offset': '0',
--- tests/calls/test_getCalls.py
+++ tests/calls/test_getCalls.py
@@ -1,10 +1,10 @@
 import pytest
 import requests
 
-from config import BASE_URI, USERNAME, PASSWORD, calls_params
+from config import BASE_URI, USERNAME, PASSWORD, CALL_PARAM
 from conftest import encoded
 from src.assertions.assertion_schemas import assert_schema_calls_without_params
-from src.assertions.assert_headers import assert_content_type_applicationJson
+from src.assertions.assertion_headers import assert_content_type_applicationJson
 from src.assertions.assertion_status import assert_status_code_ok, assert_status_code_unauthorized, \
     assert_status_bad_request, assert_status_code_internal_server_error
 from src.espocrm_api.api_request import EspoCRMRequest
@@ -51,21 +51,21 @@ def test_get_calls_invalid_authorization_in_headers(get_header_cookie):
 def test_get_calls_with_param_select_maxsize_offset_orderby_order_default(get_header_cookie):
     url = f"{BASE_URI}/Call?"
     headers = get_header_cookie(USERNAME, PASSWORD)
-    response = requests.get(url, params=calls_params, headers=headers)
+    response = requests.get(url, params=CALL_PARAM, headers=headers)
     assert_status_code_ok(response)
 
 def test_get_calls_with_param_select_otherValidData(get_header_cookie):
     url = f"{BASE_URI}/Call?"
     headers = get_header_cookie(USERNAME, PASSWORD)
-    calls_params['select'] = 'dateStart%2dateEnd%Cstatus%2Cname%Cid'
-    response = requests.get(url, params=calls_params, headers=headers)
+    CALL_PARAM['select'] = 'dateStart%2dateEnd%Cstatus%2Cname%Cid'
+    response = requests.get(url, params=CALL_PARAM, headers=headers)
     assert_status_code_ok(response)
 
 def test_get_calls_with_param_select_invalidData(get_header_cookie):
     url = f"{BASE_URI}/Call?"
     headers = get_header_cookie(USERNAME, PASSWORD)
-    calls_params['select'] = 'testCallsTest'
-    response = requests.get(url, params=calls_params, headers=headers)
+    CALL_PARAM['select'] = 'testCallsTest'
+    response = requests.get(url, params=CALL_PARAM, headers=headers)
     assert_status_bad_request(response)
     assert_status_code_internal_server_error(response)
     #This test case fails, and its correct because the field sent in 'select" doesn´t exist therefore
@@ -74,24 +74,24 @@ def test_get_calls_with_param_select_invalidData(get_header_cookie):
 def test_get_calls_with_param_select_empty(get_header_cookie):
     url = f"{BASE_URI}/Call?"
     headers = get_header_cookie(USERNAME, PASSWORD)
-    response = requests.get(url, params=calls_params, headers=headers)
-    if 'select' in calls_params:
-        del calls_params['select']
-    response = requests.get(url, params=calls_params, headers=headers)
+    response = requests.get(url, params=CALL_PARAM, headers=headers)
+    if 'select' in CALL_PARAM:
+        del CALL_PARAM['select']
+    response = requests.get(url, params=CALL_PARAM, headers=headers)
     assert_status_code_ok(response)
 
 def test_get_calls_with_param_maxsize_otherValidData(get_header_cookie):
     url = f"{BASE_URI}/Call?"
     headers = get_header_cookie(USERNAME, PASSWORD)
-    calls_params['maxsize'] = '5'
-    response = requests.get(url, params=calls_params, headers=headers)
+    CALL_PARAM['maxsize'] = '5'
+    response = requests.get(url, params=CALL_PARAM, headers=headers)
     assert_status_code_ok(response)
 
 def test_get_calls_with_param_maxsize_invalidData(get_header_cookie):
     url = f"{BASE_URI}/Call?"
     headers = get_header_cookie(USERNAME, PASSWORD)
-    calls_params['maxsize'] = ('2032+-*-/*-489589438345')
-    response = requests.get(url, params=calls_params, headers=headers)
+    CALL_PARAM['maxsize'] = ('2032+-*-/*-489589438345')
+    response = requests.get(url, params=CALL_PARAM, headers=headers)
     assert_status_bad_request(response)
     assert_status_code_internal_server_error(response)
     #This test case fails, and its correct because the field sent in 'select" doesn´t exist therefore
@@ -100,24 +100,24 @@ def test_get_calls_with_param_maxsize_invalidData(get_header_cookie):
 def test_get_calls_with_param_maxsize_empty(get_header_cookie):
     url = f"{BASE_URI}/Call?"
     headers = get_header_cookie(USERNAME, PASSWORD)
-    response = requests.get(url, params=calls_params, headers=headers)
-    if 'maxsize' in calls_params:
-        del calls_params['maxsize']
-    response = requests.get(url, params=calls_params, headers=headers)
+    response = requests.get(url, params=CALL_PARAM, headers=headers)
+    if 'maxsize' in CALL_PARAM:
+        del CALL_PARAM['maxsize']
+    response = requests.get(url, params=CALL_PARAM, headers=headers)
     assert_status_code_ok(response)
 
 def test_get_calls_with_param_offset_otherValidData(get_header_cookie):
     url = f"{BASE_URI}/Call?"
     headers = get_header_cookie(USERNAME, PASSWORD)
-    calls_params['offset'] = '1'
-    response = requests.get(url, params=calls_params, headers=headers)
+    CALL_PARAM['offset'] = '1'
+    response = requests.get(url, params=CALL_PARAM, headers=headers)
     assert_status_code_ok(response)
 
 def test_get_calls_with_param_offset_invalidData(get_header_cookie):
     url = f"{BASE_URI}/Call?"
     headers = get_header_cookie(USERNAME, PASSWORD)
-    calls_params['offset'] = ('lpo/*-!"$$/%&(//&)(%$$<<')
-    response = requests.get(url, params=calls_params, headers=headers)
+    CALL_PARAM['offset'] = ('lpo/*-!"$$/%&(//&)(%$$<<')
+    response = requests.get(url, params=CALL_PARAM, headers=headers)
     assert_status_bad_request(response)
     assert_status_code_internal_server_error(response)
     #This test case fails, and its correct because the field sent in 'select" doesn´t exist therefore
@@ -126,55 +126,55 @@ def test_get_calls_with_param_offset_invalidData(get_header_cookie):
 def test_get_calls_with_param_offset_empty(get_header_cookie):
     url = f"{BASE_URI}/Call?"
     headers = get_header_cookie(USERNAME, PASSWORD)
-    response = requests.get(url, params=calls_params, headers=headers)
-    if 'offset' in calls_params:
-        del calls_params['offset']
-    response = requests.get(url, params=calls_params, headers=headers)
+    response = requests.get(url, params=CALL_PARAM, headers=headers)
+    if 'offset' in CALL_PARAM:
+        del CALL_PARAM['offset']
+    response = requests.get(url, params=CALL_PARAM, headers=headers)
     assert_status_code_ok(response)
 
 def test_get_calls_with_param_orderBy_otherValidData(get_header_cookie):
     url = f"{BASE_URI}/Call?"
     headers = get_header_cookie(USERNAME, PASSWORD)
-    calls_params['orderBy'] = 'dateEnd'
-    response = requests.get(url, params=calls_params, headers=headers)
+    CALL_PARAM['orderBy'] = 'dateEnd'
+    response = requests.get(url, params=CALL_PARAM, headers=headers)
     assert_status_code_ok(response)
 
 def test_get_calls_with_param_orderBY_invalidData(get_header_cookie):
     url = f"{BASE_URI}/Call?"
     headers = get_header_cookie(USERNAME, PASSWORD)
-    calls_params['orderBy'] = ('lpo/*-!"$$/%&(//&)(%$$<<')
-    response = requests.get(url, params=calls_params, headers=headers)
+    CALL_PARAM['orderBy'] = ('lpo/*-!"$$/%&(//&)(%$$<<')
+    response = requests.get(url, params=CALL_PARAM, headers=headers)
     assert_status_bad_request(response)
 
 def test_get_calls_with_param_orderBy_empty(get_header_cookie):
     url = f"{BASE_URI}/Call?"
     headers = get_header_cookie(USERNAME, PASSWORD)
-    response = requests.get(url, params=calls_params, headers=headers)
-    if 'orderBy' in calls_params:
-        del calls_params['orderBy']
-    response = requests.get(url, params=calls_params, headers=headers)
+    response = requests.get(url, params=CALL_PARAM, headers=headers)
+    if 'orderBy' in CALL_PARAM:
+        del CALL_PARAM['orderBy']
+    response = requests.get(url, params=CALL_PARAM, headers=headers)
     assert_status_code_ok(response)
 
 def test_get_calls_with_param_order_otherValidData(get_header_cookie):
     url = f"{BASE_URI}/Call?"
     headers = get_header_cookie(USERNAME, PASSWORD)
-    calls_params['order'] = 'asc'
-    response = requests.get(url, params=calls_params, headers=headers)
+    CALL_PARAM['order'] = 'asc'
+    response = requests.get(url, params=CALL_PARAM, headers=headers)
     assert_status_code_ok(response)
 
 def test_get_calls_with_param_order_invalidData(get_header_cookie):
     url = f"{BASE_URI}/Call?"
     headers = get_header_cookie(USERNAME, PASSWORD)
-    calls_params['order'] = ('lpo/*-!"$$/%&(//&)(%$$<<')
-    response = requests.get(url, params=calls_params, headers=headers)
+    CALL_PARAM['order'] = ('lpo/*-!"$$/%&(//&)(%$$<<')
+    response = requests.get(url, params=CALL_PARAM, headers=headers)
 
     assert_status_bad_request(response)
 
 def test_get_calls_with_param_order_empty(get_header_cookie):
     url = f"{BASE_URI}/Call?"
     headers = get_header_cookie(USERNAME, PASSWORD)
-    response = requests.get(url, params=calls_params, headers=headers)
-    if 'order' in calls_params:
-        del calls_params['order']
-    response = requests.get(url, params=calls_params, headers=headers)
+    response = requests.get(url, params=CALL_PARAM, headers=headers)
+    if 'order' in CALL_PARAM:
+        del CALL_PARAM['order']
+    response = requests.get(url, params=CALL_PARAM, headers=headers)
     assert_status_code_ok(response)
