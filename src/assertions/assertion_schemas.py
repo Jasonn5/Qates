3af86041514c11ab954e0579dd431ca7704c6d81
Diego Claros <cardenas.ericka1697@gmail.com>
2024-06-29 22:10:12 -0400

    SCRUM-16:Post_Email_Send email
    se añadio test cases a la carpeta de test y los aserts

--- src/assertions/assertion_schemas.py
+++ src/assertions/assertion_schemas.py
@@ -3,7 +3,17 @@ import jsonschema
 import json
 from jsonschema import validate
 
+
+
 def assert_schema_presentation(instance):
     with open("src/resources/schemas/presentation.json", "r") as schema_file:
         schema = json.load(schema_file)
     validate(instance=instance, schema=schema)
+
+def assert_schema_correoImportant(instance):
+    with open("src/resources/schemas/correoImportant.json", "r") as schema_file:
+        schema = json.load(schema_file)
+    validate(instance=instance, schema=schema)
+
+
+
