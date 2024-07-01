e31982e06f52d3ae1b634d47819003df1a5857a4
Ericka Cardenas <cardenas.ericka1697@gmail.com>
2024-07-01 12:39:09 -0400

    cambios en el emal_Impotant_param

--- config.py
+++ config.py
@@ -8,16 +8,6 @@ CALL_PARAM = {
     'orderBy': 'dateStart',
     'order': 'desc'
 }
-EMAIL_IMPORTAN_PARAM = {
-    "select": "dateSent,parentId,parentType,parentName,subject,personStringData",
-    "maxSize": 20,
-    "offset": 0,
-    "orderBy": "dateSent",
-    "order": "desc",
-    "where[0][type]": "inFolder",
-    "where[0][attribute]": "folderId",
-    "where[0][value]": "important"
-}
 
 MEETING_PARAM = {
     'select': 'id,name,status,dateStart,dateEnd,dateStartDate,dateEndDate,parentId,parentType,parentName,createdById,assignedUserId,assignedUserName',
