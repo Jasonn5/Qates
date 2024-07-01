eda95f5d71790aaeb67073fc2c1ff061954710ea
Diego-Claros17 <lclarosrocha@gmail.com>
2024-07-01 10:43:35 -0400

    restructuring of the code according to the new structure

--- tests/Correo_borrador/test_GetEmail.py
+++ tests/Correo_borrador/test_GetEmail.py
@@ -5,113 +5,85 @@ from src.assertions.assertion_status import (
     assert_status_code_unauthorized,
     assert_status_bad_request
 )
-
+from src.resources.auth.auth import Auth
+from src.espocrm_api.email_endpoints import EndpointEmail
 
 def test_get_email_succed(get_headers):
-    url = f"{BASE_URI}/Email?select=2CparentType&maxSize=20&offset=0&order=desc&where%5B0%5D%5Btype%5D=inFolder&where%5B0%5D%5Battribute%5D=folderId&where%5B0%5D%5Bvalue%5D=drafts"
-    response = requests.get(url, headers=get_headers("admin", "admin"))
+    url = f"{BASE_URI}{EndpointEmail.GET_EMAIL_WITHOUT_PARAMS.value}"
+    response = requests.get(url, headers=Auth().auth_valid_credential(get_headers))
     assert_status_code_ok(response)
 
 def test_get_all_drafts_status_code_200(get_headers):
-    url = f"{BASE_URI}/Email?select=dateSent,parentId,parentType,parentName,subject,personStringData&maxSize=20&offset=0&orderBy=dateSent&order=desc&where[0][type]=inFolder&where[0][attribute]=folderId&where[0][value]=drafts"
-    headers = get_headers("admin", "admin")
+    url = f"{BASE_URI}{EndpointEmail.GET_EMAIL_WITH_PARAMS.value}"
+    headers = Auth().auth_valid_credential(get_headers)
     response = requests.get(url, headers=headers)
     assert_status_code_ok(response)
 
 def test_get_drafts_with_wrong_basic_auth(get_headers):
-    url = f"{BASE_URI}/Email?select=dateSent,parentId,parentType,parentName,subject,personStringData&maxSize=20&offset=0&orderBy=dateSent&order=desc&where[0][type]=inFolder&where[0][attribute]=folderId&where[0][value]=drafts"
-    headers = get_headers("wrong", "credentials")
+    url = f"{BASE_URI}{EndpointEmail.GET_EMAIL_WITH_PARAMS.value}"
+    headers = Auth().auth_invalid_credentials(get_headers)
     response = requests.get(url, headers=headers)
     assert_status_code_unauthorized(response)
 
-#Is a bug?
 def test_get_drafts_without_auth_token_secret(get_headers):
-    url = f"{BASE_URI}/Email?select=dateSent,parentId,parentType,parentName,subject,personStringData&maxSize=20&offset=0&orderBy=dateSent&order=desc&where[0][type]=inFolder&where[0][attribute]=folderId&where[0][value]=drafts"
-    headers = get_headers("admin", "admin")
+    url = f"{BASE_URI}{EndpointEmail.GET_EMAIL_WITH_PARAMS.value}"
+    headers = Auth().auth_valid_credential(get_headers)
     if "cookie" in headers:
         headers.pop("cookie")
     response = requests.get(url, headers=headers)
-    print(f"Request Headers: {response.request.headers}")
-    print(f"Request URL: {response.request.url}")
-    print(f"Response Status Code: {response.status_code}")
-    print(f"Response Text: {response.text}")
     assert_status_code_unauthorized(response)
-#Is a bug?
+
 def test_get_drafts_with_wrong_content_type(get_headers):
-    url = f"{BASE_URI}/Email?select=dateSent,parentId,parentType,parentName,subject,personStringData&maxSize=20&offset=0&orderBy=dateSent&order=desc&where[0][type]=inFolder&where[0][attribute]=folderId&where[0][value]=drafts"
-    headers = get_headers("admin", "admin")
+    url = f"{BASE_URI}{EndpointEmail.GET_EMAIL_WITH_PARAMS.value}"
+    headers = Auth().auth_valid_credential(get_headers)
     headers["Content-Type"] = "text/plain"
     response = requests.get(url, headers=headers)
-    print(f"Request Headers: {response.request.headers}")
-    print(f"Request URL: {response.request.url}")
-    print(f"Response Status Code: {response.status_code}")
-    print(f"Response Text: {response.text}")
     assert_status_bad_request(response)
 
-#Is a bug?
 def test_get_drafts_with_additional_parameters(get_headers):
-    url = f"{BASE_URI}/Email?select=dateSent,parentId,parentType,parentName,subject,personStringData&maxSize=20&offset=0&orderBy=dateSent&order=desc&where[0][type]=inFolder&where[0][attribute]=folderId&where[0][value]=drafts&extra_param=value"
-    headers = get_headers("admin", "admin")
+    url = f"{BASE_URI}{EndpointEmail.GET_EMAIL_WITH_PARAMS.value}&extra_param=value"
+    headers = Auth().auth_valid_credential(get_headers)
     response = requests.get(url, headers=headers)
-    print(f"Request Headers: {response.request.headers}")
-    print(f"Request URL: {response.request.url}")
-    print(f"Response Status Code: {response.status_code}")
-    print(f"Response Text: {response.text}")
     assert_status_bad_request(response)
 
 def test_get_drafts_with_empty_search_param(get_headers):
-    url = f"{BASE_URI}/Email?select=dateSent,parentId,parentType,parentName,subject,personStringData&maxSize=20&offset=0&orderBy=dateSent&order=desc&where[0][type]=inFolder&where[0][attribute]=folderId&where[0][value]=drafts&search="
-    headers = get_headers("admin", "admin")
+    url = f"{BASE_URI}{EndpointEmail.GET_EMAIL_WITH_PARAMS.value}&search="
+    headers = Auth().auth_valid_credential(get_headers)
     response = requests.get(url, headers=headers)
     assert_status_code_ok(response)
 
-#Is a bug?
 def test_get_drafts_with_invalid_search_fields(get_headers):
-    url = f"{BASE_URI}/Email?select=dateSent,parentId,parentType,parentName,subject,personStringData&maxSize=20&offset=0&orderBy=dateSent&order=desc&where[0][type]=inFolder&where[0][attribute]=folderId&where[0][value]=drafts&search=invalid_field"
-    headers = get_headers("admin", "admin")
+    url = f"{BASE_URI}{EndpointEmail.GET_EMAIL_WITH_PARAMS.value}&search=invalid_field"
+    headers = Auth().auth_valid_credential(get_headers)
     response = requests.get(url, headers=headers)
-    print(f"Request Headers: {response.request.headers}")
-    print(f"Request URL: {response.request.url}")
-    print(f"Response Status Code: {response.status_code}")
-    print(f"Response Text: {response.text}")
     assert_status_bad_request(response)
 
 def test_get_drafts_with_empty_maxsize_param(get_headers):
-    url = f"{BASE_URI}/Email?select=dateSent,parentId,parentType,parentName,subject,personStringData&maxSize=&offset=0&orderBy=dateSent&order=desc&where[0][type]=inFolder&where[0][attribute]=folderId&where[0][value]=drafts"
-    headers = get_headers("admin", "admin")
+    url = f"{BASE_URI}{EndpointEmail.GET_EMAIL_WITH_PARAMS.value}&maxSize="
+    headers = Auth().auth_valid_credential(get_headers)
     response = requests.get(url, headers=headers)
     assert_status_code_ok(response)
 
 def test_get_drafts_with_valid_maxsize_param(get_headers):
-    url = f"{BASE_URI}/Email?select=dateSent,parentId,parentType,parentName,subject,personStringData&maxSize=10&offset=0&orderBy=dateSent&order=desc&where[0][type]=inFolder&where[0][attribute]=folderId&where[0][value]=drafts"
-    headers = get_headers("admin", "admin")
+    url = f"{BASE_URI}{EndpointEmail.GET_EMAIL_WITH_PARAMS.value}&maxSize=10"
+    headers = Auth().auth_valid_credential(get_headers)
     response = requests.get(url, headers=headers)
     assert_status_code_ok(response)
 
-#Is a bug?
 def test_get_drafts_with_invalid_maxsize_param(get_headers):
-    url = f"{BASE_URI}/Email?select=dateSent,parentId,parentType,parentName,subject,personStringData&maxSize=invalid&offset=0&orderBy=dateSent&order=desc&where[0][type]=inFolder&where[0][attribute]=folderId&where[0][value]=drafts"
-    headers = get_headers("admin", "admin")
+    url = f"{BASE_URI}{EndpointEmail.GET_EMAIL_WITH_PARAMS.value}&maxSize=invalid"
+    headers = Auth().auth_valid_credential(get_headers)
     response = requests.get(url, headers=headers)
-    print(f"Request Headers: {response.request.headers}")
-    print(f"Request URL: {response.request.url}")
-    print(f"Response Status Code: {response.status_code}")
-    print(f"Response Text: {response.text}")
     assert_status_bad_request(response)
 
-#Is a bug?
 def test_get_drafts_with_invalid_offset_type(get_headers):
-    url = f"{BASE_URI}/Email?select=dateSent,parentId,parentType,parentName,subject,personStringData&maxSize=20&offset=not_an_integer&orderBy=dateSent&order=desc&where[0][type]=inFolder&where[0][attribute]=folderId&where[0][value]=drafts"
-    headers = get_headers("admin", "admin")
+    url = f"{BASE_URI}{EndpointEmail.GET_EMAIL_WITH_PARAMS.value}&offset=not_an_integer"
+    headers = Auth().auth_valid_credential(get_headers)
     response = requests.get(url, headers=headers)
-    print(f"Request Headers: {response.request.headers}")
-    print(f"Request URL: {response.request.url}")
-    print(f"Response Status Code: {response.status_code}")
-    print(f"Response Text: {response.text}")
     assert_status_bad_request(response)
 
 def test_get_drafts_with_invalid_order_type(get_headers):
-    url = f"{BASE_URI}/Email?select=dateSent,parentId,parentType,parentName,subject,personStringData&maxSize=20&offset=0&orderBy=dateSent&order=invalid&where[0][type]=inFolder&where[0][attribute]=folderId&where[0][value]=drafts"
-    headers = get_headers("admin", "admin")
+    url = f"{BASE_URI}{EndpointEmail.GET_EMAIL_WITH_PARAMS.value}&order=invalid"
+    headers = Auth().auth_valid_credential(get_headers)
     response = requests.get(url, headers=headers)
     assert_status_bad_request(response)
