4646caf1da0913d0f07f206b1f90833a048cb14b
Ericka Cardenas <cardenas.ericka1697@gmail.com>
2024-07-08 16:42:10 -0400

    Añadiendo mis test cases

--- api_endpoints/mail_important_endpoints.py
+++ api_endpoints/mail_important_endpoints.py
@@ -5,3 +5,4 @@ class EndpointCorreoImportant(Enum):
     GET_CORREO_INVALID_ORDER = "/Email?select=dateSent%2Csubject%2CfromName&maxSize=20&offset=0&orderBy=dateSent&order=invalidOrder"
     GET_CORREO_MAXSIZE_1 = "/Email?select=dateSent%2Csubject%2CfromName&maxSize=1&offset=0&orderBy=dateSent&order=desc"
     GET_CORREO_OFFSET_1000 = "/Email?select=dateSent%2Csubject%2CfromName&maxSize=20&offset=1000&orderBy=dateSent&order=desc"
+    POST_EMAIL_INSERT_IMAGE = "/Attachment"
