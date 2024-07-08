c2f45e431b69b9f249fd3a6534ed0e631e43d860
FreddyFZ17 <201704849@est.umss.edu>
2024-07-08 18:50:30 -0400

    TC´s Delete

--- tests/task/test_delete_task.py
+++ tests/task/test_delete_task.py
@@ -1,9 +1,9 @@
 import pytest
 
 from api_endpoints.api_request import EspoCRMRequest
-from assertions.assertion_status import assert_status_code_ok, assert_status_bad_request, assert_status_code_unauthorized, assert_status_code_method_not_allowed, assert_status_code_not_found
+from assertions.assertion_status import assert_status_code_ok, assert_status_code_unauthorized, assert_status_code_not_found
 from api_endpoints.task_ednpoints import TaskEnpoints
-from tests.task.conftest import delete_task, create_task
+from tests.task.conftest import create_task
 from resources.auth.auth import Auth
 from payloads.tasks.payload_tasks import payload_task
 
@@ -45,33 +45,3 @@ def test_delete_task_invalid_token(get_headers):
     response = EspoCRMRequest.delete(url, headers)
     assert_status_code_unauthorized(response)
 
-'''@pytest.mark.functional
-@pytest.mark.regression
-def test_method_not_allowed(get_headers):
-    url = TaskEnpoints.post_task_without_params()
-    headers = Auth().auth_valid_credential(get_headers)
-    response = EspoCRMRequest.delete(url, headers)
-    assert_status_code_method_not_allowed(response)'''
-
-'''@pytest.mark.functional
-@pytest.mark.regression
-def test_post_task_schema_payload(get_headers):
-    url = TaskEnpoints.post_task_without_params()
-    headers = Auth().auth_valid_credential(get_headers)
-    response = EspoCRMRequest.post(url, headers, payload_task)
-    assert_payload_schema_task(response.json())'''
-
-'''@pytest.mark.functional
-@pytest.mark.regression
-def test_post_task_missing_optional_fields(get_headers):
-    url = TaskEnpoints.post_task_without_params()
-    headers = Auth().auth_valid_credential(get_headers)
-    response = EspoCRMRequest.post(url, headers, payload_task)
-    assert_status_code_ok(response)
-    delete_task(payload_task['id'], get_headers)'''
-
-'''@pytest.mark.functional
-@pytest.mark.regression
-def test_post_task_duplicate_creation(setup_teardown_task, get_headers):
-    response = setup_teardown_task
-    assert_status_bad_request(response)'''
