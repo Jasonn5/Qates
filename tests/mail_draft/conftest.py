67e8bd2c4eade25d4f6ce9a2305cb6bf77d48ba9
Diego-Claros17 <lclarosrocha@gmail.com>
2024-07-09 01:18:50 -0400

    TCS added

--- tests/mail_draft/conftest.py
+++ tests/mail_draft/conftest.py
@@ -4,51 +4,11 @@ import requests
 from config.config import BASE_URI, USERNAME, PASSWORD
 from api_endpoints.mail_draft_endpoints import EndpointEmail
 
-
-@pytest.fixture(scope="function", autouse=True)
-def teardown():
-    print("Setup: Test is starting")
-    yield
-    print("Teardown: Test has ended. Starting cleanup.")
-    delete_created_drafts()
-    print("Teardown: Cleanup finished")
-
-
 def encoded(username, password):
     credentials = f"{username}:{password}"
     encoded_credentials = base64.b64encode(credentials.encode('utf-8')).decode('utf-8')
     print(f"Encoded Credentials: {encoded_credentials}")
     return encoded_credentials
-
-
-def delete_created_drafts():
-    print("Teardown: Preparing to delete drafts")
-    headers = {
-        'Authorization': f'Basic {encoded(USERNAME, PASSWORD)}',
-        'Content-Type': 'application/json'
-    }
-
-    print(f"Teardown: Headers prepared for deletion: {headers}")
-
-    url = f"{BASE_URI}{EndpointEmail.GET_EMAIL_WITH_PARAMS.value}"
-    response = requests.get(url, headers=headers)
-
-    print(f"Teardown: Response status code: {response.status_code}, Response: {response.text}")
-
-    if response.status_code == 200:
-        drafts = response.json().get('list', [])
-        for draft in drafts:
-            draft_id = draft['id']
-            delete_url = f"{BASE_URI}{EndpointEmail.DELETE_EMAIL_DRAFT.value.format(id=draft_id)}"
-            delete_response = requests.delete(delete_url, headers=headers)
-            print(f"Teardown: Delete draft ID {draft_id} response status code: {delete_response.status_code}")
-            if delete_response.status_code == 200:
-                print(f"Teardown: Draft ID {draft_id} deleted successfully")
-            else:
-                print(f"Teardown: Failed to delete draft ID {draft_id}")
-    else:
-        print(f"Teardown: Failed to retrieve drafts, status code: {response.status_code}, response: {response.text}")
-
 
 @pytest.fixture
 def get_headers():
@@ -63,33 +23,24 @@ def get_headers():
 
     return _get_headers
 
+@pytest.fixture(scope="function", autouse=True)
+def teardown():
+    print("Setup: Test is starting")
+    yield
+    print("Teardown: Test has ended. Starting cleanup.")
+    delete_created_drafts()
+    print("Teardown: Cleanup finished")
 
-@pytest.fixture(scope="function")
-def setup_email_draft(get_headers):
-    url = f"{BASE_URI}{EndpointEmail.POST_EMAIL_DRAFT.value}"
-    headers = get_headers(USERNAME, PASSWORD)
-    payload_success = {
-        "subject": "Test Draft",
-        "body": "This is a test draft",
-        "from": "test@example.com",
-        "to": ["recipient@example.com"]
-    }
-    response = requests.post(url, headers=headers, json=payload_success)
-
-    if response.status_code != 201:
-        print(f"Error: Received status code {response.status_code}")
-        print(f"Response content: {response.text}")
-        pytest.fail(f"Setup failed: Unable to create email draft. Status code: {response.status_code}")
-
-    draft_id = response.json().get('id')
-    yield draft_id
-    delete_draft(draft_id)  # Cleanup after test
-
-
-def delete_draft(draft_id):
+def delete_created_drafts():
     headers = {
         'Authorization': f'Basic {encoded(USERNAME, PASSWORD)}',
         'Content-Type': 'application/json'
     }
-    delete_url = f"{BASE_URI}{EndpointEmail.DELETE_EMAIL_DRAFT.value.format(id=draft_id)}"
-    requests.delete(delete_url, headers=headers)
+    url = f"{BASE_URI}{EndpointEmail.GET_EMAIL_WITH_PARAMS.value}"
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        drafts = response.json().get('list', [])
+        for draft in drafts:
+            draft_id = draft['id']
+            delete_url = f"{BASE_URI}{EndpointEmail.DELETE_EMAIL_DRAFT.value.format(id=draft_id)}"
+            requests.delete(delete_url, headers=headers)
--- tests/mail_draft/test_delete_email_draft.py
+++ tests/mail_draft/test_delete_email_draft.py
@@ -3,71 +3,65 @@ import requests
 from config.config import BASE_URI, USERNAME, PASSWORD
 from api_endpoints.mail_draft_endpoints import EndpointEmail
 
+
+@pytest.fixture(scope="function")
+def setup_email_draft(get_headers):
+    url = f"{BASE_URI}{EndpointEmail.POST_EMAIL_DRAFT.value}"
+    headers = get_headers(USERNAME, PASSWORD)
+    payload_success = {
+        "subject": "Test Draft",
+        "body": "This is a test draft",
+        "from": "test@example.com",
+        "to": ["recipient@example.com"]
+    }
+    response = requests.post(url, headers=headers, json=payload_success)
+
+    if response.status_code != 201:
+        print(f"Error: Received status code {response.status_code}")
+        print(f"Response content: {response.text}")
+        pytest.fail(f"Setup failed: Unable to create email draft. Status code: {response.status_code}")
+
+    draft_id = response.json().get('id')
+    yield draft_id
+    delete_draft(draft_id)
+
+
+def delete_draft(draft_id):
+    headers = {
+        'Authorization': f'Basic {encoded(USERNAME, PASSWORD)}',
+        'Content-Type': 'application/json'
+    }
+    delete_url = f"{BASE_URI}{EndpointEmail.DELETE_EMAIL_DRAFT.value.format(id=draft_id)}"
+    requests.delete(delete_url, headers=headers)
+
+
 @pytest.mark.smoke
 @pytest.mark.functional
 @pytest.mark.regression
 def test_delete_email_draft_success(get_headers, setup_email_draft):
-    draft_id = setup_email_draft
+    url = f"{BASE_URI}{EndpointEmail.DELETE_EMAIL_DRAFT.value.format(id=setup_email_draft)}"
     headers = get_headers(USERNAME, PASSWORD)
+    response = requests.delete(url, headers=headers)
+    assert response.status_code == 200
 
-    # Probar la eliminación del borrador
-    delete_url = f"{BASE_URI}{EndpointEmail.DELETE_EMAIL_DRAFT.value.format(id=draft_id)}"
-    delete_response = requests.delete(delete_url, headers=headers)
-    assert delete_response.status_code == 200
 
 @pytest.mark.smoke
 @pytest.mark.functional
 @pytest.mark.regression
 def test_delete_email_draft_no_auth_header(setup_email_draft):
-    draft_id = setup_email_draft
-    headers_no_auth = {
-        'Content-Type': 'application/json'
-    }
+    url = f"{BASE_URI}{EndpointEmail.DELETE_EMAIL_DRAFT.value.format(id=setup_email_draft)}"
+    response = requests.delete(url)
+    assert response.status_code == 401
 
-    # Probar la eliminación del borrador sin header de autorización
-    delete_url = f"{BASE_URI}{EndpointEmail.DELETE_EMAIL_DRAFT.value.format(id=draft_id)}"
-    delete_response = requests.delete(delete_url, headers=headers_no_auth)
-    assert delete_response.status_code == 401
 
 @pytest.mark.smoke
 @pytest.mark.functional
 @pytest.mark.regression
 def test_delete_email_draft_invalid_auth_token(setup_email_draft):
-    draft_id = setup_email_draft
-    headers_invalid_auth = {
-        'Authorization': 'Basic invalid_token',
+    url = f"{BASE_URI}{EndpointEmail.DELETE_EMAIL_DRAFT.value.format(id=setup_email_draft)}"
+    headers = {
+        'Authorization': 'Basic invalidtoken',
         'Content-Type': 'application/json'
     }
-
-    # Probar la eliminación del borrador con un token de autorización inválido
-    delete_url = f"{BASE_URI}{EndpointEmail.DELETE_EMAIL_DRAFT.value.format(id=draft_id)}"
-    delete_response = requests.delete(delete_url, headers=headers_invalid_auth)
-    assert delete_response.status_code == 401
-
-@pytest.mark.smoke
-@pytest.mark.functional
-@pytest.mark.regression
-def test_delete_email_draft_expired_auth_token(setup_email_draft):
-    draft_id = setup_email_draft
-    headers_expired_auth = {
-        'Authorization': 'Basic expired_token',
-        'Content-Type': 'application/json'
-    }
-
-    # Probar la eliminación del borrador con un token de autorización expirado (Simulado)
-    delete_url = f"{BASE_URI}{EndpointEmail.DELETE_EMAIL_DRAFT.value.format(id=draft_id)}"
-    delete_response = requests.delete(delete_url, headers=headers_expired_auth)
-    assert delete_response.status_code == 401
-
-@pytest.mark.smoke
-@pytest.mark.functional
-@pytest.mark.regression
-def test_delete_email_draft_response_content_type(get_headers, setup_email_draft):
-    draft_id = setup_email_draft
-    headers = get_headers(USERNAME, PASSWORD)
-
-    # Probar la eliminación del borrador y verificar el tipo de contenido en la respuesta
-    delete_url = f"{BASE_URI}{EndpointEmail.DELETE_EMAIL_DRAFT.value.format(id=draft_id)}"
-    delete_response = requests.delete(delete_url, headers=headers)
-    assert delete_response.status_code == 200
-    assert delete_response.headers['Content-Type'] == 'application/json'
+    response = requests.delete(url, headers=headers)
+    assert response.status_code == 401
