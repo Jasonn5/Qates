fca038954421f7d23a23eb2651185cadf2c1c83b
Ericka Cardenas <cardenas.ericka1697@gmail.com>
2024-07-09 02:25:55 -0400

    resolviendo errores de los payloads

--- tests/mail_important/test_post_email_insert_image.py
+++ tests/mail_important/test_post_email_insert_image.py
@@ -1,3 +1,4 @@
+
 import pytest
 import allure
 from api_endpoints.api_request import EspoCRMRequest
@@ -6,22 +7,20 @@ from assertions.assertion_status import *
 from assertions.assertion_schemas import assert_valid_schema, email_insert_image_schema
 from config.config import BASE_URI, USERNAME, PASSWORD
 from resources.auth.auth import Auth
-from payloads.mail_important.payload_email_insert_image import *
 import requests
-
 
 @pytest.mark.smoke
 @pytest.mark.functional
 @pytest.mark.regression
 @allure.feature('Email Insert Image')
 @allure.story('Post Email Insert Image')
-def test_post_email_insert_image_success(get_headers):
+def test_post_email_insert_image_success(get_headers, teardown_email, valid_email_payload):
     """
     Verify successful addition of image to email - status code 200 OK
     """
     url = f"{BASE_URI}{EndpointCorreoImportant.POST_EMAIL_INSERT_IMAGE.value}"
     headers = Auth().auth_valid_credential(get_headers)
-    response = EspoCRMRequest.post_json(url, headers, email_insert_image_payload)
+    response = EspoCRMRequest.post_json(url, headers, valid_email_payload)
     assert_status_code_ok(response)
     data = response.json()
     assert_valid_schema(data, email_insert_image_schema)
@@ -31,7 +30,7 @@ def test_post_email_insert_image_success(get_headers):
 @pytest.mark.regression
 @allure.feature('Email Insert Image')
 @allure.story('Post Email Insert Image')
-def test_post_email_insert_image_invalid_email_id(get_headers):
+def test_post_email_insert_image_invalid_email_id(get_headers, invalid_email_id_payload):
     """
     Verify addition of image to email with invalid email ID - status code 400 Bad Request
     """
@@ -46,7 +45,7 @@ def test_post_email_insert_image_invalid_email_id(get_headers):
 @pytest.mark.regression
 @allure.feature('Email Insert Image')
 @allure.story('Post Email Insert Image')
-def test_post_email_insert_image_without_auth_header(get_headers):
+def test_post_email_insert_image_without_auth_header(get_headers, email_insert_image_payload):
     """
     Verify addition of image to email without authorization header - status code 401
     """
@@ -61,7 +60,7 @@ def test_post_email_insert_image_without_auth_header(get_headers):
 @pytest.mark.regression
 @allure.feature('Email Insert Image')
 @allure.story('Post Email Insert Image')
-def test_post_email_insert_image_invalid_auth_token(get_headers):
+def test_post_email_insert_image_invalid_auth_token(get_headers, email_insert_image_payload):
     """
     Verify addition of image to email with invalid authorization token - status code 401
     """
@@ -76,7 +75,7 @@ def test_post_email_insert_image_invalid_auth_token(get_headers):
 @pytest.mark.regression
 @allure.feature('Email Insert Image')
 @allure.story('Post Email Insert Image')
-def test_post_email_insert_image_network_timeout(get_headers, monkeypatch):
+def test_post_email_insert_image_network_timeout(get_headers, email_insert_image_payload, monkeypatch):
     """
     Verify addition of image to email with network timeout - status code 504 Gateway Timeout
     """
@@ -97,7 +96,7 @@ def test_post_email_insert_image_network_timeout(get_headers, monkeypatch):
 @pytest.mark.regression
 @allure.feature('Email Insert Image')
 @allure.story('Post Email Insert Image')
-def test_post_email_insert_image_check_response_content_type(get_headers):
+def test_post_email_insert_image_check_response_content_type(get_headers, email_insert_image_payload):
     """
     Verify addition of image to email and check response content type - status code 200 OK
     """
@@ -112,7 +111,7 @@ def test_post_email_insert_image_check_response_content_type(get_headers):
 @pytest.mark.regression
 @allure.feature('Email Insert Image')
 @allure.story('Post Email Insert Image')
-def test_post_email_insert_image_large_image_size(get_headers):
+def test_post_email_insert_image_large_image_size(get_headers, large_image_payload):
     """
     Verify addition of image to email with large image size - status code 413 Request Entity Too Large
     """
@@ -126,7 +125,7 @@ def test_post_email_insert_image_large_image_size(get_headers):
 @pytest.mark.regression
 @allure.feature('Email Insert Image')
 @allure.story('Post Email Insert Image')
-def test_post_email_insert_image_empty_request_body(get_headers):
+def test_post_email_insert_image_empty_request_body(get_headers, empty_body_payload):
     """
     Verify addition of image to email with empty request body - status code 400 Bad Request
     """
@@ -140,7 +139,7 @@ def test_post_email_insert_image_empty_request_body(get_headers):
 @pytest.mark.regression
 @allure.feature('Email Insert Image')
 @allure.story('Post Email Insert Image')
-def test_post_email_insert_image_unauthorized_user(get_headers):
+def test_post_email_insert_image_unauthorized_user(get_headers, email_insert_image_payload):
     """
     Verify addition of image to email by unauthorized user - status code 403 Forbidden
     """
@@ -154,7 +153,7 @@ def test_post_email_insert_image_unauthorized_user(get_headers):
 @pytest.mark.regression
 @allure.feature('Email Insert Image')
 @allure.story('Post Email Insert Image')
-def test_post_email_insert_image_server_down(get_headers, monkeypatch):
+def test_post_email_insert_image_server_down(get_headers, email_insert_image_payload, monkeypatch):
     """
     Verify addition of image to email when server is down - status code 503 Service Unavailable
     """
@@ -176,7 +175,7 @@ def test_post_email_insert_image_server_down(get_headers, monkeypatch):
 @pytest.mark.regression
 @allure.feature('Email Insert Image')
 @allure.story('Post Email Insert Image')
-def test_post_email_insert_image_ensure_all_data_in_response(get_headers):
+def test_post_email_insert_image_ensure_all_data_in_response(get_headers, email_insert_image_payload):
     """
     Verify addition of image to email and ensure all associated data is included in the response - status code 200 OK
     """
