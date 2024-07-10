0047fe53da73ad1e0e641829cb844259eebf1456
Jeyson Erik Valdivia Bernal <jasson.n021@gmail.com>
2024-07-10 18:04:57 -0400

    ss

--- tests/mail_important/test_delete_email_important.py
+++ tests/mail_important/test_delete_email_important.py
@@ -1,4 +1,4 @@
-import pytest
+'''import pytest
 import allure
 from api.request.api_request import EspoCRMRequest
 from core.config.config import BASE_URI
@@ -84,4 +84,4 @@ def test_delete_email_important_check_response_content_type(get_headers, create_
     headers = Auth().auth_valid_credential(get_headers)
     response = EspoCRMRequest.delete(url, headers)
     assert_status_code_ok(response)
-    assert response.headers['Content-Type'] == 'application/json'
+    assert response.headers['Content-Type'] == 'application/json'''
